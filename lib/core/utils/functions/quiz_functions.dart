b5772695d46c69e2937fd4616417334652689241
AYHM13x <ayhmalakel1995@gmail.com>
2023-11-18 21:04:07 +0300

    add get color func file

--- lib/core/utils/functions/quiz_functions.dart
+++ lib/core/utils/functions/quiz_functions.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../features/_2_quiz/presentation/model_view/all_questions_cubit/all_questions_cubit.dart';
-import '../app_colors.dart';
-import '../cubits/settings_cubit/settings_cubit.dart';
 
 void goToNextQuestion(BuildContext context) {
   BlocProvider.of<AllQuestionsCubit>(context).goToNextQuestion();
@@ -11,37 +9,6 @@ void goToNextQuestion(BuildContext context) {
 
 void submitAnswer(BuildContext context) {
   BlocProvider.of<AllQuestionsCubit>(context).updateScore();
-}
-
-Color getBackGroundColorAfterSubmit({
-  required int id,
-  required String currentChoosedAnwser,
-  required int choosedAnswerIndex,
-  required bool isDark,
-}) {
-  if (currentChoosedAnwser == "true") {
-    return AppColors.greenColor;
-  } else {
-    if (id == choosedAnswerIndex) {
-      return AppColors.redColor;
-    } else {
-      if (isDark) {
-        return AppColors.blackColor;
-      } else {
-        return AppColors.whiteColor;
-      }
-    }
-  }
-}
-
-Color getColorDifficulty(String text) {
-  if (text == "Easy") {
-    return AppColors.greenColor;
-  } else if (text == "Medium") {
-    return AppColors.orangeColor;
-  } else {
-    return AppColors.redColor;
-  }
 }
 
 String getTheFinalResultTextView(BuildContext context) {
@@ -62,52 +29,4 @@ double getNumberOfAvailbleAnswers(List<String?> list) {
     }
   }
   return x;
-}
-
-Color getShapeColorAswerItem(BuildContext context, bool isSelected) {
-  if (isSelected) {
-    return AppColors.orangeColor;
-  } else {
-    if (BlocProvider.of<SettingsCubit>(context).getApptheme()) {
-      return AppColors.whiteColor;
-    } else {
-      return AppColors.blackColor;
-    }
-  }
-}
-
-Color getBackGroundColorAswerItem(BuildContext context, bool isSelected) {
-  if (isSelected) {
-    return AppColors.yellowColor;
-  } else {
-    if (BlocProvider.of<SettingsCubit>(context).getApptheme()) {
-      return AppColors.blackColor;
-    } else {
-      return AppColors.whiteColor;
-    }
-  }
-}
-
-Color getTextColorAswerItem(BuildContext context, bool isSelected) {
-  if (isSelected) {
-    return AppColors.blackColor;
-  } else {
-    if (BlocProvider.of<SettingsCubit>(context).getApptheme()) {
-      return AppColors.whiteColor;
-    } else {
-      return AppColors.blackColor;
-    }
-  }
-}
-
-Color getColorTextAfterSubmit({
-  required bool isSelected,
-  required String isCorrect,
-  required bool isDark,
-}) {
-  if ((isSelected && isCorrect == "false") || isCorrect == "true" || !isDark) {
-    return AppColors.blackColor;
-  } else {
-    return AppColors.whiteColor;
-  }
 }
