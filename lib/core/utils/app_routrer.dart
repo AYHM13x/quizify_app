71f8df2247f9260d2fcf96d2eae8ac20612eb1d6
AYHM13x <ayhmalakel1995@gmail.com>
2023-11-21 00:46:30 +0300

    add navigation to aboutme view

--- lib/core/utils/app_routrer.dart
+++ lib/core/utils/app_routrer.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quizify_app/features/_0_splash/presentation/views/splash_view.dart';
 import 'package:quizify_app/features/_1_home/presentation/views/about_me/_0_about_me_view.dart';
 
-import '../../features/_0_splash/presentation/views/splash_view.dart';
 import '../../features/_1_home/presentation/views/home_view.dart';
 import '../../features/_2_quiz/presentation/views/quiz_view.dart';
 
@@ -10,17 +10,22 @@ abstract class AppRouter {
   //Paths
   static String splashViewPath = "/";
   static String homeViewPath = "/homeView";
+  static String aboutMeViewPath = "/homeView/AboutMeViewPath";
   static String quizViewPath = "/homeView/QuizView";
 
   static final RouterConfig<Object> router = GoRouter(
     routes: [
       GoRoute(
         path: splashViewPath,
-        builder: (context, state) => const AboutMeView(),
+        builder: (context, state) => const SplashView(),
       ),
       GoRoute(
         path: homeViewPath,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: aboutMeViewPath,
+        builder: (context, state) => const AboutMeView(),
       ),
       GoRoute(
         path: quizViewPath,
